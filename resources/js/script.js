68815806b30a8cadff4204a938dda48d90201335
Matheus Canabarro <matheus.canabarro@hotmail.com>
2018-03-22 15:08:25 -0300

    Mobile Main Nav effects using jQuery

--- resources/js/script.js
+++ resources/js/script.js
@@ -16,7 +16,7 @@ $(document).ready(function() {
         $('html, body').animate({srollTop: $('.js--section-plans').offset().top}, 1000);
     });
 
-    $('.js--scroll-to-features ').click(function () {
+    $('.js--scroll-to-start').click(function () {
         $('html, body').animate({srollTop: $('.js--section-features').offset().top}, 1000);
     });
 
@@ -81,5 +81,21 @@ $(document).ready(function() {
       $('.js--wp-4').addClass('animated pulse');
     }, {
       offset: '50%'
+    });
+
+    /* MOBILE NAV */
+    $('.js--nav-icon').click(function(){
+      var nav = $('.js--main-nav');
+      var icon = $('.js--nav-icon i');
+
+      nav.slideToggle(200);
+
+      if (icon.hasClass('ion-navicon-round')) {
+        icon.addClass('ion-close-round');
+        icon.removeClass('ion-navicon-round');
+      } else {
+        icon.addClass('ion-navicon-round');
+        icon.removeClass('ion-close-round');
+      }
     });
 });
